123915c399b2173d2e1d0aa02b119eaa67ed88a5
gopal6148 <gopalbangale2000@gmail.com>
2025-08-12 11:33:22 +0530

    Create FertilizerShopLocationApplication.java

---
+++ FertilizerShopLoca/FertilizerShopLocation/src/main/java/com/shopLocation/FertilizerShopLocation/FertilizerShopLocationApplication.java
@@ -0,0 +1,13 @@
+package com.shopLocation.FertilizerShopLocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FertilizerShopLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FertilizerShopLocationApplication.class, args);
+	}
+
+}
