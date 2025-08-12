e18b4b9c7322c9c332a1ff4b2bc99cb629873513
gopal6148 <gopalbangale2000@gmail.com>
2025-08-12 11:33:08 +0530

    Create LocationRequest.java

---
+++ FertilizerShopLoca/FertilizerShopLocation/src/main/java/com/shopLocation/FertilizerShopLocation/dto/LocationRequest.java
@@ -0,0 +1,33 @@
+package com.shopLocation.FertilizerShopLocation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LocationRequest {
+	private double latitude;
+    private double longitude;
+	public double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	public double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	public LocationRequest() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+    
+
+}
