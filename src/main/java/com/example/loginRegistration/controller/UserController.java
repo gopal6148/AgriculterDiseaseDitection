8a0559595e637a1ba1f3e1b7e0416d32251fe756
gopal6148 <132437812+gopal6148@users.noreply.github.com>
2026-05-22 17:14:33 +0530

    some changes on controller class

--- src/main/java/com/example/loginRegistration/controller/UserController.java
+++ src/main/java/com/example/loginRegistration/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.example.loginRegistration.entity.User;
 import com.example.loginRegistration.service.UserService;
 import com.example.loginRegistration.util.JWTUtil;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/auth")
 public class UserController {
