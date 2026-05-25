3acf574afd568d6928182707af63c2e1ddf87d84
Gopal Ambadas Bangale <132437812+gopal6148@users.noreply.github.com>
2026-05-25 18:36:30 +0530

    Delete User-Service directory
    i have create other

--- User-Service/src/main/java/com/example/User_Service/exception/UserAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.example.User_Service.exception;
-
-public class UserAlreadyExistsException extends RuntimeException{
-	
-	public UserAlreadyExistsException(String massage) {
-		super(massage);
-	}
-
-}
